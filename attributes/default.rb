50ede8ac835a9a6da1f7fd1f88592fff153c5002
Stephen Baldwin <stephenbaldwin@me.com>
2013-05-28 18:33:59 -0400

    typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 include_attribute 'deploy'
 default[:interspire][:opsworks_autofs_map_file] = '/etc/auto.opsworks'
-default[:interspire][:path] = "/mnt/srv/administration_panel/shared/interspire"
+default[:interspire][:path] = "/mnt/srv/www/administration_panel/shared/interspire"
 default[:interspire][:ec2_path] = "/vol/interspire/interspire"
