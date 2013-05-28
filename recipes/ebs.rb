f61ff01c039c6e6c185a4bf9bb7b9565ff37d9f6
Stephen Baldwin <stephenbaldwin@me.com>
2013-05-28 15:58:37 -0400

    deploy

--- recipes/ebs.rb
+++ recipes/ebs.rb
@@ -1,10 +1,13 @@
-Chef::Log.info("Skipping Interspire EBS setup - using what is already on the EBS volume")
+node[:deploy].each do |application, deploy|
+	deploy = node[:deploy][application]
+	Chef::Log.info("Skipping Interspire EBS setup - using what is already on the EBS volume")
 
-bash "adding bind mount for cache, config to #{node[:interspire][:opsworks_autofs_map_file]}" do
-  user 'root'
-  code <<-EOC
-    echo "#{deploy[:deploy_to]}/shared/interspire -fstype=none,bind,rw :#{node[:interspire][:ec2_path]}" >> #{node[:interspire][:opsworks_autofs_map_file]}
-    service autofs restart
-  EOC
-  not_if { ::File.read("#{node[:interspire][:opsworks_autofs_map_file]}").include?("#{deploy[:deploy_to]}/shared/interspire") }
+	bash "adding bind mount for cache, config to #{node[:interspire][:opsworks_autofs_map_file]}" do
+		user 'root'
+		code <<-EOC
+			echo "#{deploy[:deploy_to]}/shared/interspire -fstype=none,bind,rw :#{node[:interspire][:ec2_path]}" >> #{node[:interspire][:opsworks_autofs_map_file]}
+			service autofs restart
+		EOC
+		not_if { ::File.read("#{node[:interspire][:opsworks_autofs_map_file]}").include?("#{deploy[:deploy_to]}/shared/interspire") }
+	end
 end
