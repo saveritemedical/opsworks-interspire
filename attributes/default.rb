c4ef451ecec58ac8cfbc2574a6fda91b6f27bb47
Stephen Baldwin <stephenbaldwin@me.com>
2013-05-28 15:45:34 -0400

    ebs mount

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,5 @@
 include_attribute 'deploy'
+default[:interspire][:opsworks_autofs_map_file] = '/etc/auto.opsworks'
+default[:interspire][:path][:config] = "config"
+default[:interspire][:path][:cache] = "cache"
+default[:interspire][:ec2_path] = "/vol/interspire"
---
+++ recipes/ebs.rb
@@ -0,0 +1,10 @@
+Chef::Log.info("Skipping Interspire EBS setup - using what is already on the EBS volume")
+
+bash "adding bind mount for cache, config to #{node[:interspire][:opsworks_autofs_map_file]}" do
+  user 'root'
+  code <<-EOC
+    echo "#{deploy[:deploy_to]}/shared/interspire -fstype=none,bind,rw :#{node[:interspire][:ec2_path]}" >> #{node[:interspire][:opsworks_autofs_map_file]}
+    service autofs restart
+  EOC
+  not_if { ::File.read("#{node[:interspire][:opsworks_autofs_map_file]}").include?("#{deploy[:deploy_to]}/shared/interspire") }
+end
