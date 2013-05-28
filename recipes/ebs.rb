15714bafea99d0c1d793d1a8641b25992f1138c2
Stephen Baldwin <stephenbaldwin@me.com>
2013-05-28 17:15:59 -0400

    hardcode

--- recipes/ebs.rb
+++ recipes/ebs.rb
@@ -3,8 +3,8 @@ Chef::Log.info("Skipping Interspire EBS setup - using what is already on the EBS
 bash "adding bind mount for cache, config to #{node[:interspire][:opsworks_autofs_map_file]}" do
 	user 'root'
 	code <<-EOC
-		echo "/srv/www/administration_panel/shared/interspire -fstype=none,bind,rw :/vol/interspire" >> #{node[:interspire][:opsworks_autofs_map_file]}
+		echo '/srv/www/administration_panel/shared/interspire -fstype=none,bind,rw :/vol/interspire' >> /etc/auto.opsworks
 		service autofs restart
 	EOC
-	not_if { ::File.read("#{node[:interspire][:opsworks_autofs_map_file]}").include?("/srv/www/administration_panel/shared/interspire") }
+	not_if { ::File.read("/etc/auto.opsworks").include?("/srv/www/administration_panel/shared/interspire") }
 end
